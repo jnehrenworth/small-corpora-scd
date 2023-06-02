9472219ebaa61fbbdbb2d228db40bacce01588c6
Jackson Ehrenworth <jne1@williams.edu>
2023-06-02 08:53:23 -0400

    Final experiment update

--- experiments.py
+++ experiments.py
@@ -47,7 +47,7 @@ def yield_results(
 
 def run_stability_experiment(corpora_path: str, results_path: str):
     TARGET_TKNS = 150_000
-    EXPS = 6
+    EXPS = 500
 
     fns_pack = {
         'UWB': evaluation_rules_UWB,
@@ -62,7 +62,7 @@ def run_stability_experiment(corpora_path: str, results_path: str):
         if results_file.readline().strip("\n") != "Language,Model,Spearman's Rho":
             writer.writerow(["Language", "Model", "Spearman's Rho"])
 
-        for language in ["swedish"]:
+        for language in ["english", "german", "swedish"]:
             result_packs = yield_results(corpora_path, TARGET_TKNS, EXPS, language, fns_pack)
             for model_name, result in result_packs:
                 writer.writerow([language, model_name, result])
@@ -78,19 +78,13 @@ def run_token_size_experiment(corpora_path: str, results_path: str):
         'temporal_attention': evaluation_rules_TA
     }
 
-    token_test_targets = range(5_750_000, 6_250_000+1, 500_000)
+    token_test_targets = range(250_000, 6_250_000+1, 500_000)
     with open(results_path, "a+") as results_file:
         writer = csv.writer(results_file, delimiter=",")
 
         results_file.seek(0)
         if results_file.readline().strip("\n") != "Language,Model,Tokens,Spearman's Rho":
             writer.writerow(["Language", "Model", "Tokens", "Spearman's Rho"])
-
-        for model_name, result in yield_results(corpora_path, 5250000, 1, "english", {"temporal_attention": evaluation_rules_TA}):
-            writer.writerow(["english", model_name, 5250000, result])
-
-        for model_name, result in yield_results(corpora_path, 5250000, 1, "english", fns_pack):
-            writer.writerow(["english", model_name, 5250000, result])
 
         for token_target in token_test_targets:
             result_packs = yield_results(corpora_path, token_target, EXPS_PER_TKN_TRGT, "english", fns_pack)
@@ -104,7 +98,7 @@ def run_experiments():
     results_dir = "results"
     Path(results_dir).mkdir(exist_ok=True, parents=True)
 
-    # run_stability_experiment(corpora_path, f"{results_dir}/stability_experiment.csv")
+    run_stability_experiment(corpora_path, f"{results_dir}/stability_experiment.csv")
     run_token_size_experiment(corpora_path, f"{results_dir}/token_size_experiment.csv")
 
 if __name__ == "__main__":
